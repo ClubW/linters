fb831a3bbdef70255c034f838764350389bfd80f
Mark Thomas <markthethomas@gmail.com>
2016-02-11 10:24:54 -0800

    feature: finish basic interface and functionality

---
+++ lint.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const inquirer = require("inquirer");
+const path = require('path');
+const _ = require('lodash')
+const fs = require('fs');
+
+
+let projectType = {
+  type: 'list',
+  name: 'projectType',
+  default: 'eslint',
+  message: 'Which linting config do you want to set up?',
+  choices: fs.readdirSync('./lib')
+};
+
+
+inquirer.prompt([projectType], function(choice) {
+  let targetFileExists = _.find(choice.projectType);
+  if (targetFileExists) {
+      fs.createReadStream(`./lib/${choice.projectType}`).pipe(fs.createWriteStream(choice.projectType));
+  }
+});
